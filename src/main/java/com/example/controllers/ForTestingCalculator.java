c461f58345376e85f4d9511422743ceff41605e4
Dima Olaba <defire04st@gmail.com>
2022-08-28 20:16:03 +0300

    Added class for testing the calculator

---
+++ src/main/java/com/example/controllers/ForTestingCalculator.java
@@ -0,0 +1,7 @@
+package com.example.controllers;
+
+public class ForTestingCalculator {
+    public static void main(String[] args) {
+        System.out.println(Calculator.preparingExpression("5+2/-4"));
+    }
+}
